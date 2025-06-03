1c8a6d993953aa365623089c22a971d3190c92fb
Vaaruna2709 <vaaruna35@gmail.com>
2025-06-03 18:45:14 +0530

    fixed bugs

--- init/index.js
+++ init/index.js
@@ -11,7 +11,7 @@ main()
   .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect('mongodb://127.0.0.1:27017/bookMyStay');
 
 }
 async function initDB() {
