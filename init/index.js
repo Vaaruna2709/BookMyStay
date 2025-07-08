5f5e937ee13b401544573ca9fce75b87f42f4a93
Vaaruna2709 <vaaruna35@gmail.com>
2025-07-08 22:55:12 +0530

    url changes

--- init/index.js
+++ init/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
@@ -11,8 +12,11 @@ main()
   .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/bookMyStay');
-
+ // await mongoose.connect('mongodb://127.0.0.1:27017/bookMyStay');
+ await  mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 }
 async function initDB() {
   try {
